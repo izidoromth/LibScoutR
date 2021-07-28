fbd466693534778e6d1b5e00fb15bc43ac377719
NEOGIGIO <gigio.forastieri@gmail.com>
2021-07-28 16:49:08 -0300

    create movement on library logic

---
+++ software/misc/robot.py
@@ -0,0 +1,15 @@
+"""
+ _     _ _     _____                 _  ______ 
+| |   (_) |   /  ___|               | | | ___ \
+| |    _| |__ \ `--.  ___ ___  _   _| |_| |_/ /
+| |   | | '_ \ `--. \/ __/ _ \| | | | __|    / 
+| |___| | |_) /\__/ / (_| (_) | |_| | |_| |\ \ 
+\_____/_|_.__/\____/ \___\___/ \__,_|\__\_| \_|
+
+"""
+
+from library import Library
+
+lib = Library()
+print(lib.find_path("Red", "Detective"))
+# print(lib.generate_categories_edges("Adventure"))
