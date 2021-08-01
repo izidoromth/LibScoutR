6ac8b315a57ca82c497b1502d351d580d17b46cc
NEOGIGIO <gigio.forastieri@gmail.com>
2021-07-31 21:25:06 -0300

    fully working helper mode

--- software/misc/arduino_interface.py
+++ software/misc/arduino_interface.py
@@ -1,3 +1,6 @@
+import time
+
+
 class ArduinoInterface:
     def __init__(self):
         self.__done = True
@@ -8,5 +11,6 @@ class ArduinoInterface:
     def goto(self, came_from_color, current_color, next_color, scan=False):
         self.__done = False
         while self.__done == False:
-            input("Moving. Scanning = {0}".format(scan))
-            return
+            print("Moving. Scanning = {0}".format(scan))
+            time.sleep(1)
+            self.done()
