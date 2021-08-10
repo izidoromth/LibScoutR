0b5aaab0bd037fc34738125d3b89b414b41cfc7f
DSCsmw <dscsmw@ymail.com>
2021-08-10 19:44:34 -0300

    turn qr_detection into a function

--- software/misc/qr_detection_standalone.py
+++ software/misc/qr_detection_standalone.py
@@ -4,58 +4,88 @@ import numpy as np
 import pyzbar
 from pyzbar.pyzbar import decode, ZBarSymbol
 
-DISPLAY_CAPTURE = True
 
-if(DISPLAY_CAPTURE):
-    cv2.namedWindow("preview")
+"""  ----- Qr Code Detection -----
 
-# open camera feed
-vc = cv2.VideoCapture('http://192.168.0.2:8080/video')
+camera_address:         address of the camera
+exposition_iterations:  how many frames to be captured
+crop_top:               distance from top to be cropped
+crop_bottom:            distance from bottom to be cropped
+display_capture:        wether to display the preview window or not
 
-# try to get the first frame
-if vc.isOpened():
-    rval, img = vc.read()
-else:
-    rval = False
-
-detected_qr_list = []
-while rval:
-    # detect qr codes
-    decoded = decode(img, symbols=[ZBarSymbol.QRCODE])
+returns a list with the codes detected, in order from left to right  """
 
 
-    for qr in decoded:
-        # get info for each qr code
-        text_qr = qr.data.decode("utf-8")
-        points = np.array(qr.polygon, np.int32)
-        x_pos, y_pos = points[1]
+def qr_detect(camera_address, exposition_iterations, crop_top, crop_bottom, display_capture):
 
-        # check if qr already exists in the list
-        if not (text_qr in (item for sublist in detected_qr_list for item in sublist)):
-            # add text and x position to list
-            detected_qr_list.append([text_qr, x_pos])
-            # order detected qr list
-            detected_qr_list.sort(key=lambda x: x[1])
+    if(display_capture):
+        cv2.namedWindow("preview")
 
-        if (DISPLAY_CAPTURE):
-            # draw polygon
-            cv2.polylines(img, [points], 1, (255,0,0),4)
-            # draw text
-            cv2.putText(img, text_qr, (x_pos, y_pos), cv2.FONT_HERSHEY_SIMPLEX, .75, (0, 0, 0), 4)
+    # open camera feed
+    vc = cv2.VideoCapture(camera_address)
+
+    # try to get the first frame
+    if vc.isOpened():
+        rval, img = vc.read()
+    else:
+        rval = False
+
+    detected_qr_list = []
+    iterations_count = 0
+    while (rval and iterations_count < exposition_iterations):
+        iterations_count += 1
+
+        # crop image
+        height, width, channels = img.shape
+        img = img[crop_top:height-crop_bottom, 0:width]
+
+        # detect qr codes
+        decoded = decode(img, symbols=[ZBarSymbol.QRCODE])
+
+        for qr in decoded:
+            # get info for each qr code
+            text_qr = qr.data.decode("utf-8")
+            points = np.array(qr.polygon, np.int32)
+            x_pos, y_pos = points[1]
+
+            # check if qr already exists in the list
+            if not (text_qr in (item for sublist in detected_qr_list for item in sublist)):
+                # add text and x position to list
+                detected_qr_list.append([text_qr, x_pos])
+                # order detected qr list
+                detected_qr_list.sort(key=lambda x: x[1])
+
+            if (display_capture):
+                # draw polygon
+                cv2.polylines(img, [points], 1, (255,0,0),4)
+                # draw text
+                cv2.putText(img, text_qr, (x_pos, y_pos), cv2.FONT_HERSHEY_SIMPLEX, .75, (0, 0, 0), 4)
 
 
-    if (DISPLAY_CAPTURE):
-        # draw list of detected codes at top left corner
-        i=0
-        for qr in detected_qr_list:
-            cv2.putText(img, qr[0], (5,25+i), cv2.FONT_HERSHEY_SIMPLEX, .75, (255, 0, 255), 2)
-            i+=25
+        if (display_capture):
+            # draw list of detected codes at top left corner
+            i=0
+            for qr in detected_qr_list:
+                cv2.putText(img, qr[0], (5,25+i), cv2.FONT_HERSHEY_SIMPLEX, .75, (255, 0, 255), 2)
+                i+=25
 
-        cv2.imshow("preview", img)
-        key = cv2.waitKey(1)
+            cv2.imshow("preview", img)
+            key = cv2.waitKey(1)
+
+        rval, img = vc.read()
+
 
     # prepare output list
     out_list = []
     out_list = [qr[0] for qr in detected_qr_list]
 
-    rval, img = vc.read()
+    return out_list
+
+
+
+# Usage example
+print (qr_detect(camera_address = 'http://192.168.0.2:8080/video',
+                 exposition_iterations = 50,
+                 crop_top = 150,
+                 crop_bottom = 150,
+                 display_capture = True))
