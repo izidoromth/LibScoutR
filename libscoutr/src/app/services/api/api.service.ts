30b1f4fe70d90078fb87b400f691221589451a3a
gabriell56 <46854106+gabriell56@users.noreply.github.com>
2021-08-13 01:35:11 -0300

    Create libscoutr

---
+++ libscoutr/src/app/services/api/api.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Book } from '../../models/book';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private baseUrl:string = 'http://localhost:5000/'
+
+  constructor(private http: HttpClient) { }
+
+  getBooks(){
+    return this.http.get<Book[]>(this.baseUrl+'original_books')
+  };
+
+}
