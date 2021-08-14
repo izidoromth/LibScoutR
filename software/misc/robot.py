b1f9a86f363cf3ad2713438c26ba7d1959e03778
NEOGIGIO <gigio.forastieri@gmail.com>
2021-08-13 22:33:57 -0300

    edit library organize shelve and scout

--- software/misc/robot.py
+++ software/misc/robot.py
@@ -235,30 +235,20 @@ class Robot:
         else:
             print("\n\n")
 
-    def read_shelve(self):
-        list_of_codes_read = [
-            "102.1x",  # Ed Sheeran
-            "2321.23",  # Alan Turing
-            "43442.aa2",  # Steve Jobs
-            "7542.69",  # Lupin
-            "7543.69",  # Scooby Doo
-        ]
-        return list_of_codes_read
-
-    def organize_shelve(self, category):
-        codes_read = self.read_shelve()
+    def organize_shelve(self, category, list_of_book_read):
+        codes_read = list_of_book_read
         books_read = self.__library.create_book_list_from_code_list(codes_read)
         same_category_books = self.__library.filter_book_list_by_category(
             books_read, category
         )
         wrong_shelf_books = [
-            book for book in books_read if book not in same_category_books
+            book.get_universal_code() for book in books_read if book not in same_category_books
         ]
         correct_books = self.__LIS(same_category_books)
         incorrect_books = []
         for book in same_category_books:
             if book not in correct_books[0]:
-                incorrect_books.append(book)
+                incorrect_books.append(book.get_universal_code())
 
         print("Oraganization plan:")
         return {"Wrong Shelve": wrong_shelf_books, "Out of Order": incorrect_books}
@@ -304,6 +294,11 @@ class Robot:
         if self.__scanning:
             # {'1st floor': 'Adventure', '2nd floor': 'Romance'}
             scanning_these_categories = self.__library.get_categories_from_color_position(self.__current_color, self.__going_to_color,)
+            
+            # {'Wrong Shelve': ['7542.69', '7543.69'], 'Out of Order': ['2321.23']}
+            # self.organize_shelve(scanning_these_categories['1st floor'], codes_scanned['1st floor'])
+            
+            # codes_scanned look like this:
             # {
             #   '1st floor': ['123 x23 iqw', '456 y12 8kk', '801 sin cos'],
             #   '2nd floor': ['123 x23 iqw', '456 y12 8kk', '801 sin cos']
